510d32585db0b50480558cc5657019fcfba24fa5
Nicolas Labrot <nithril@gmail.com>
2016-09-29 23:32:17 +0200

    Add component search criteria

--- src/main/java/com/ownspec/center/OsCenterApplication.java
+++ src/main/java/com/ownspec/center/OsCenterApplication.java
@@ -11,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-
 public class OsCenterApplication {
 
 	public static void main(String[] args) {
