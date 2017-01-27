772dc74d1c26f560926668afe7e11f32eaaaeb73
lyrold <lyrold.careto@gmail.com>
2017-01-27 08:26:13 +0100

    bootstrapping controller + dummy data

--- src/main/resources/dummy/dummyData.sql
+++ src/main/resources/dummy/dummyData.sql
@@ -24,26 +24,35 @@ VALUES(5, 'l.careto@ownspec.com','$2a$06$5uTQvTwHAHkVqUaVakAB6ui0ljYU5R0W5Qzjhj9
 
 
 -- Projects
--- Insert into "project"(id,created_date, title, description, created_user_id, manager_id)
--- values(10,'2016-10-17 17:34:17.782000', 'JPMichel', 'Standardization project', 5, 5);
---
--- Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
--- values(20,'2016-11-18 17:34:17.782000', 'OS-center', 'Req and spec centralization module', 4, 3);
---
--- Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
--- values(30,'2016-12-19 09:35:17.782000', 'OS-validation', 'Req test and validation module', 2, 4);
---
--- Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
--- values(40,'2016-12-19 10:37:17.782000', 'TravailZen', 'Reprise impression', 5, 5);
---
---
+Insert into "project"(id,created_date, title, description, created_user_id, manager_id)
+values(10,'2016-10-17 17:34:17.782000', 'JPMichel', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis', 5, 5);
+
+Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
+values(20,'2016-11-18 17:34:17.782000', 'OS-center', 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur', 4, 3);
+
+Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
+values(30,'2016-12-19 09:35:17.782000', 'OS-validation', 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur', 2, 4);
+
+Insert into "project"(id,created_date,title, description, created_user_id, manager_id)
+values(40,'2016-12-19 10:37:17.782000', 'TravailZen', 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint', 5, 5);
+
+
 -- -- User's projects
--- INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
--- VALUES (10, '2016-12-17 17:34:17.782000', false, '2016-12-17 17:40:52.062000', 2, 10, 5);
--- INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
--- VALUES (20, '2016-12-17 17:34:21.551000', false, '2016-12-17 17:34:21.551000', 1, 20, 5);
--- INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
--- VALUES (30, '2016-12-17 17:34:23.520000', true, '2016-12-17 17:34:23.520000', 1, 30, 5);
--- INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
--- VALUES (40, '2016-12-17 17:34:26.155000', false, '2016-12-17 17:34:26.155000', 1, 40, 5);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (10, '2016-12-17 17:34:17.782000', false, '2016-12-17 17:40:52.062000', 2, 10, 5);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (20, '2016-12-17 17:34:21.551000', false, '2016-12-17 17:34:21.551000', 1, 20, 5);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (30, '2016-12-17 17:34:23.520000', true, '2016-12-17 17:34:23.520000', 1, 30, 5);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (40, '2016-12-17 17:34:26.155000', false, '2016-12-17 17:34:26.155000', 1, 40, 5);
+
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (50, '2016-12-17 17:34:17.782000', false, '2016-12-17 17:40:52.062000', 2, 10, 1);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (60, '2016-12-17 17:34:21.551000', false, '2016-12-17 17:34:21.551000', 1, 20, 1);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (70, '2016-12-17 17:34:23.520000', true, '2016-12-17 17:34:23.520000', 1, 30, 1);
+INSERT INTO user_project (id, created_date, favorite, last_modified_date, visited_time, project_id, user_id)
+VALUES (80, '2016-12-17 17:34:26.155000', false, '2016-12-17 17:34:26.155000', 1, 40, 1);
 
